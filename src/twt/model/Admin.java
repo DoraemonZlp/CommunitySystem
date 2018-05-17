1d1094990937694dee4834536029ece9068acede
知 未 <i237731947@outlook.com>
2018-05-17 11:16:21 +0800

    456

--- src/twt/model/Admin.java
+++ src/twt/model/Admin.java
@@ -4,7 +4,7 @@ public class Admin {
 	public int id;
 	public String name;
 	public String phone;
-	public String t1;//113
+	public String t1;//1234
 	
 	public Admin(int id, String name, String phone) {
 		super();
