a3b86a1497cfbc4cabe1e27f39e3914094aa4e6d
知 未 <i237731947@outlook.com>
2018-05-24 12:46:18 +0800

    修改hibernate配置文件

--- src/twt/UnitTest/Test1.java
+++ src/twt/UnitTest/Test1.java
@@ -1,10 +1,44 @@
 package twt.UnitTest;
 
-public class Test1 {
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
+import twt.model.Admin;
+
+public class Test1 {
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//
+		// 实例化Configuration
+		Configuration configuration = new Configuration();
+		// 加载hibernate.cfg.xml文件
+		configuration.configure("/hibernate.cfg.xml");
+		// 创建SessionFactory
+		// Hibernate4.3创建SessionFactory的方式
+		StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder();
+		standardServiceRegistryBuilder.applySettings(configuration
+				.getProperties());
+		SessionFactory sessionFactory = configuration
+				.buildSessionFactory(standardServiceRegistryBuilder.build());
+		// 打开Session
+		Session session = sessionFactory.openSession();
+		// 开始一个事务
+		Transaction trans = session.beginTransaction();
+		// 查询Customer表
+		Query query = session.createQuery("from Admin");
+		List<Admin> admins=query.list();
+		for(Admin a:admins) {
+			System.out.println(a.getName());
+			a.setName("管理员001");
+			session.save(a);  
+	        session.flush();  
+	        session.getTransaction().commit();  
+		}
+		sessionFactory.close();
 	}
 
 }
